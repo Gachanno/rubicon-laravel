64b4651a5ac6adb6c130cb1f8b2b58648c1ba7b3
Gachanno <vitana2452@gmail.com>
2026-06-11 07:31:45 +0300

    а

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Рубикон</title>
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/svg+xml" href="{{asset('/favicon.svg')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/favicon-16x16.png')}}">
+    <link rel="icon" href="{{asset('/favicon.ico')}}" sizes="any">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/apple-touch-icon.png')}}">
+    <link rel="manifest" href="{{asset('/site.webmanifest')}}">
+    <link rel="icon" type="image/png" sizes="512x512" href="{{asset('/android-chrome-512x512.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{asset('/android-chrome-192x192.png')}}">
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
